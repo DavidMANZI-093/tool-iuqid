aade1bb8fd63c3be0c62a4b8576d5209926607db
David MANZI <manzidavid093@gmail.com>
2026-02-16 00:04:45 +0200

    feat: Add main CLI entry point with flag parsing

---
+++ cmd/tool-iquid/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"flag"
+	"time"
+
+	"github.com/DavidMANZI-093/tool-iquid/pkg/core"
+	logger "github.com/DavidMANZI-093/tool-iquid/pkg/utils"
+)
+
+func main() {
+	routerURL := flag.String("url", "http://192.168.1.254", "Router URL")
+	username := flag.String("user", "", "Router Username")
+	password := flag.String("pass", "", "Router Password")
+	targetSSID := flag.String("ssid", "WiFi - The House", "Target SSID to watch")
+	cooldown := flag.Duration("cooldown", 5*time.Minute, "Cooldown time after reboot")
+	checkInterval := flag.Duration("interval", 30*time.Second, "Internet check interval")
+	dryRun := flag.Bool("dry-run", false, "Perform checks and login but do not reboot")
+	force := flag.Bool("force", false, "Force login check even if internet is online")
+	configFile := flag.String("config", "config.json", "Path to configuration file")
+
+	flag.Parse()
+
+	cfg, err := core.LoadConfig(*configFile)
+	if err == nil {
+		if *routerURL == "http://192.168.1.254" && cfg.RouterURL != "" {
+			*routerURL = cfg.RouterURL
+		}
+		if *username == "" && cfg.Username != "" {
+			*username = cfg.Username
+		}
+		if *password == "" && cfg.Password != "" {
+			*password = cfg.Password
+		}
+		if *targetSSID == "WiFi - The House" && cfg.TargetSSID != "" {
+			*targetSSID = cfg.TargetSSID
+		}
+		if *cooldown == 5*time.Minute && cfg.Cooldown != 0 {
+			*cooldown = cfg.Cooldown
+		}
+		if *checkInterval == 30*time.Second && cfg.CheckInterval != 0 {
+			*checkInterval = cfg.CheckInterval
+		}
+	}
+
+	logger.Info("Starting tool-iquid. Watching SSID: %s", *targetSSID)
+
+	for {
+		ssid, err := core.GetCurrentSSID()
+		if err != nil {
+			logger.Warn("Error getting SSID: %v", err)
+			time.Sleep(5 * time.Second)
+			continue
+		} else if ssid != *targetSSID {
+			logger.Warn("Not connected to target SSID (Current: %s). Waiting...", ssid)
+			time.Sleep(*checkInterval)
+			continue
+		} else {
+			logger.Info("Connected to ", ssid)
+		}
+
+		online := core.CheckConnectivity()
+		if online {
+			logger.Success("Internet is ONLINE")
+		} else {
+			logger.Warn("Internet is OFFLINE")
+		}
+
+		if !online || *force {
+			if *force && online {
+				logger.Warn("Force mode enabled. Initiating recovery check...")
+			} else {
+				logger.Warn("Initiating recovery sequence...")
+			}
+
+			timeout := 30 * time.Second
+			if cfg != nil && cfg.Timeout > 0 {
+				timeout = cfg.Timeout
+			}
+
+			// TODO: Continnue
+		}
+	}
+}
