dc183a4fc2538b33f5121e1571bbf9756eb950a5
David MANZI <manzidavid093@gmail.com>
2026-02-16 09:45:55 +0200

    refactor: Refactor logger to utils package with debug/error functions

--- pkg/utils/logger.go
+++ pkg/utils/logger.go
@@ -1,30 +1,34 @@
-package logger
+package utils
 
 import (
 	"log"
 )
 
 const (
-	ColorReset  = "\033[0m"
-	ColorRed    = "\033[31m"
-	ColorGreen  = "\033[32m"
-	ColorYellow = "\033[33m"
-	ColorBlue   = "\033[34m"
-	ColorCyan   = "\033[36m"
+	colorReset   = "\033[0m"
+	colorRed     = "\033[31m"
+	colorGreen   = "\033[32m"
+	colorYellow  = "\033[33m"
+	colorMagenta = "\033[35m"
+	colorCyan    = "\033[36m"
 )
 
-func Info(format string, v ...interface{}) {
-	log.Printf(ColorCyan+"(info) "+ColorReset+format, v...)
+func LogInfo(format string, v ...interface{}) {
+	log.Printf(colorCyan+"(info) "+colorReset+format, v...)
 }
 
-func Success(format string, v ...interface{}) {
-	log.Printf(ColorGreen+"(success) "+ColorReset+format, v...)
+func LogSuccess(format string, v ...interface{}) {
+	log.Printf(colorGreen+"(success) "+colorReset+format, v...)
 }
 
-func Warn(format string, v ...interface{}) {
-	log.Printf(ColorYellow+"(warn) "+ColorReset+format, v...)
+func LogWarn(format string, v ...interface{}) {
+	log.Printf(colorYellow+"(warn) "+colorReset+format, v...)
 }
 
-func Error(format string, v ...interface{}) {
-	log.Printf(ColorRed+"(error) "+ColorReset+format, v...)
+func LogError(format string, v ...interface{}) {
+	log.Printf(colorRed+"(error) "+colorReset+format, v...)
+}
+
+func LogDebug(format string, v ...interface{}) {
+	log.Printf(colorMagenta+"(error) "+colorReset+format, v...)
 }
